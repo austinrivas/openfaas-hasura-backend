37b3b44d1f5f62aa7febdaa0e4b4210508b25ae6
Austin Rivas <austinrivas@gmail.com>
2020-06-13 03:04:06 +0700

    added rust-hello demo func

--- services/rust-hello/function/src/lib.rs
+++ services/rust-hello/function/src/lib.rs
@@ -2,12 +2,12 @@
 extern crate serde_json;
 use warp::{Filter, filters::BoxedFilter, Reply};
 
-pub fn handle() -> BoxedFilter<(impl Reply,)> {
+pub fn main() -> BoxedFilter<(impl Reply,)> {
   // POST /hola => 200 OK with body
   warp::post()
     .and(warp::header("user-agent"))
     .map(|agent: String| {
-      let message = format!("Hola hasura, whose agent is {}", agent);
+      let message = format!("Hola hasura!!!! whose agent is {}", agent);
       json!({"message": message}).to_string()
     })
     .boxed()
--- services/rust-hello/template/rust-warp/function/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use warp::{Filter, filters::BoxedFilter, Reply};
-
-pub fn handle() -> BoxedFilter<(impl Reply,)> {
-  // GET /hello/warp => 200 OK with body "Hello, warp!"
-  warp::path!("hello" / String)
-    .map(|name| format!("Hello, {}!", name))
-    .boxed()
-}
